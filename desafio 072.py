48444a26948d695b59fb08befee08ec2c6da5596
LucasLessaB <lucasbragalessa123456789@gmail.com>
2026-06-23 18:02:42 -0300

    Initial commit

---
+++ desafio 072.py
@@ -0,0 +1,6 @@
+contagem = ('zero', 'um','dois','tres','quatro' , 'cinco' , 'seis' ,'sete' , 'oito','nove','dez',
+'onze','doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete','dezoito','dezenove','vinte')
+
+n = int(input('Digite um numero entre 0 e 20: '))
+print(contagem[n])
+
